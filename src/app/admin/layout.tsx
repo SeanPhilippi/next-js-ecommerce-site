d70153a77548fd849d31fbb009c7acc2750c437d
SeanPhilippi <sean.philippi@gmail.com>
2024-07-07 16:24:54 -0400

    created Nav components and imported into AdmnDashboard

---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from 'react';
+import { Nav, NavLink } from '@/components/Nav';
+
+const AdminLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
+  return (
+    <>
+      <Nav>
+        <NavLink href='/admin'>Dashboard</NavLink>
+        <NavLink href='/admin/products'>Products</NavLink>
+        <NavLink href='/admin/users'>Customer</NavLink>
+        <NavLink href='/admin/orders'>Orders</NavLink>
+      </Nav>
+      <div className='container my-6'>{children}</div>
+    </>
+  );
+};
+
+export default AdminLayout;
